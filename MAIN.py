acd01f643a64c1b43e300a65fd3c140eab7cbc9b
DennisFGardner <dennis.gardner@colorado.edu>
2017-01-04 00:01:07 -0800

    time stamp file name

--- MAIN.py
+++ MAIN.py
@@ -1,16 +1,24 @@
 # import modules
 import picamera
+import datetime
+
+
+now =datetime.datetime.now()
+
+timeStr = now.strftime('%Y%m%d_%H%M%S')
+print('Time Stamp (Year Month Day _ 24H min sec): '+timeStr)
 
 
 # create instance of the PiCamera class
 camera = picamera.PiCamera()
 
 
-# set camera settings
+# set camera settings (I want to read about these more and optimize for low light)
 camera.vflip = True
-
+camera.brightness = 50 # not sure if this is before or after the capture,
+# I have not heard of a brightness adjustment before on a caera
 
 # grab an image
-camera.capture('testImage.jpg')
+camera.capture(timeStr+'.jpg')
 
 
